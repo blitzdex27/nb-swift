b6ad63a6e93d50442e67f9e758c5070d2454cf25
dexter ramos <blitzdex27@gmail.com>
2023-03-05 18:15:05 +0800

    000-ch2-starter

--- courses/000 - Swift 5 Essential Training/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch2_Variable_Basics.playground/Pages/Variables and constants.xcplaygroundpage/Contents.swift
+++ courses/000 - Swift 5 Essential Training/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch2_Variable_Basics.playground/Pages/Variables and constants.xcplaygroundpage/Contents.swift
@@ -15,4 +15,8 @@ Declaring variables in Swift depends on how you plan to use their values in your
  [Next Topic](@next)
  
 */
+var currentHealth = 90;
+let maxHealth = 100;
 
+currentHealth = 20;
+//maxHealth = 10;
